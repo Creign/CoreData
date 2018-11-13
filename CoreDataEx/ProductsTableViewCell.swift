683980d95c43dff1c006fcb03994415db870c7e6
Excell <xcel@Excells-MacBook-Pro.local>
2018-11-13 10:04:48 +0800

    implem add delete and fetch

---
+++ CoreDataEx/ProductsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductsTableViewCell.swift
+//  CoreDataEx
+//
+//  Created by Excell on 12/11/2018.
+//  Copyright © 2018 Excell. All rights reserved.
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
